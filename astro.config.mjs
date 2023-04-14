a03743d96416151b3844b78bf62462be634d4878
NiiightmareXD <contact.aliiranpour@gmail.com>
2023-04-14 19:20:08 +0330

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: 'https://niiightmarexd.github.io/',
+  base: '/taskify-astro',
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    sitemap(),
+  ],
+});
